6499ce46cc8c09ac490209f127409eb4e7490da5
Unknown <shartman@specterlabs.com>
2018-10-13 09:09:35 -0400

    Init

---
+++ hello/main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: h3xx
+ *
+ * Created on October 13, 2018, 12:31 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World\n");
+    return 0;
+            
+
+}
+
